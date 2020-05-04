e77ee4b2162ce825ac327865a7ca7e113fb46489
NikolaiTPT <46711696+NikolaiTPT@users.noreply.github.com>
2020-05-05 00:15:50 +0300

    Add files via upload

---
+++ Terraform/variable.tf
@@ -0,0 +1,17 @@
+variable "credentials" {
+    default = "nikolai.json"
+}
+variable "project" {
+  default = "logical-bloom-276118"
+  description = "Project id"
+}
+
+variable "region" {
+  default = "europe-north1"
+  description = "region"
+}
+
+variable "zone" {
+  default = "europe-north1-a"
+  description = "zone"
+}
